51b47db2a3049320d7307564e0a49d4016772a6f
mitel <mitelone@gmail.com>
2016-04-29 10:47:05 +0400

    test15
    should work, but no auth on marathon

--- runtime/marathon/template.sh
+++ runtime/marathon/template.sh
@@ -27,7 +27,7 @@ cat > test1.json <<EOF
   }],
   "labels":{
     "HAPROXY_GROUP":"external",
-    "HAPROXY_0_VHOST":"dcos-PublicSlaveLo-1U9GDZ8EFGHCD-1052101230.us-east-1.elb.amazonaws.com",
+    "HAPROXY_0_VHOST":"dcos-no-a-PublicSl-1AV7QLK7UU4GA-393133415.us-east-1.elb.amazonaws.com",
     "HAPROXY_0_PATH": "/devops"
   }
 }
